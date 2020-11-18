29631a78946087419b64abc0b137160655a9bfc0
Nuno Santos <61665536+4E756E6F@users.noreply.github.com>
2020-11-18 23:24:08 +0000

    The start of something new
    restarted the project from scratch

--- pong.py
+++ pong.py
@@ -2,40 +2,64 @@ import pygame
 import sys
 
 
-# ? GENERAL SETUP
-pygame.init()
-clock = pygame.time.Clock()
+class Ball:
+    def __init__(self, screen, color, posX, posY, radius):
+        self.screen = screen
+        self.color = color
+        self.posX = posX
+        self.posY = posY
+        self.radius = radius
+        self.draw_ball()
 
-# ? SETTING UP THE MAIN WINDOW
-screen_width = 1280
-screen_height = 960
-screen = pygame.display.set_mode((screen_height, screen_width))
+    def draw_ball(self):
+        pygame.draw.circle(self.screen, self.color,
+                           (self.posX, self.posY), self.radius)
+
+
+class Player:
+    def __init__(self, screen, color, posX, posY, width, height):
+        self.screen = screen
+        self.color = color
+        self.posX = posX
+        self.posY = posY
+        self.width = width
+        self.height = height
+        self.draw_player()
+
+    def draw_player(self):
+        pygame.draw.rect(self.screen, self.color, (self.posX,
+                                                   self.posY, self.width, self.height))
+
+
+# ? MODULE INITIALIZER
+pygame.init()
+
+# ? CONSTANTS
+WIDTH = 1280
+HEIGHT = 960
+BACKGROUND_COLOR = (32, 32, 32)
+WHITE = (224, 224, 224)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('PONG')
 
-# ? GAME RECTANGLES
-ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
-player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
-opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
 
-bg_color = (64, 64, 64)
-whitish = (224, 224, 224)
+def paint_background():
+    screen.fill(BACKGROUND_COLOR)
+    pygame.draw.line(screen, WHITE, (WIDTH//2, 0), (WIDTH//2, HEIGHT), width=2)
 
-ball_speed_x = 10
-ball_speed_y = 10
 
+paint_background()
+
+# ? OBJECTS
+ball = Ball(screen, WHITE, WIDTH//2, HEIGHT//2, 15)
+player_left = Player(screen, WHITE, 15, HEIGHT//2-60, 20, 120)
+player_right = Player(screen, WHITE, WIDTH-20-15, HEIGHT//2-60, 20, 120)
+
+# ? MAIN LOOP
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             sys.exit()
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-    # ? VISUALS
-    screen.fill(bg_color)
-    pygame.draw.rect(screen, whitish, player)
-    pygame.draw.rect(screen, whitish, opponent)
-    pygame.draw.ellipse(screen, (51, 153, 255), ball)
-    pygame.draw.aaline(screen, whitish, (screen_width / 2, 0),
-                       (screen_width / 2, screen_height))
-    # ? UPDATING THE WINDOW
-    clock.tick(60)
+
+    pygame.display.update()
